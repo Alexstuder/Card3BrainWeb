cd7150850d82f3952e0e8c26f470fc5167a6bff0
Jollerom <jollerom@students.zhaw.ch>
2023-01-02 07:51:30 +0100

    First push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstCardComponent } from './first-card/first-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstCardComponent
   ],
   imports: [
     BrowserModule,
